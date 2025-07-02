2a7450493400f0bdd34942ea3657fcad4d36b5d1
Shakthirekak11 <shakthidharan.sib@gmail.com>
2025-07-02 18:18:36 +0530

    Update text_analysis.py

--- utils/text_analysis.py
+++ utils/text_analysis.py
@@ -11,8 +11,12 @@ import streamlit as st
 
 import spacy
 import spacy.cli
-spacy.cli.download("en_core_web_sm")
-nlp = spacy.load("en_core_web_sm")
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    spacy.cli.download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 # 🔹 Emotional & Persuasive Keywords
 emotional_words = {
